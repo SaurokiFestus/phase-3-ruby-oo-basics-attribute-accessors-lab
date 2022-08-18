d436ac6decde73acb72266ae2bf986c9cd11934b
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-08-18 11:18:14 +0300

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,34 @@
+# 
 ## code your solution here. 
+## attr_reader creates a getter method
+## attr_writer creates a setter method for us
+
+# class Person
+
+    # attr_reader :name
+    # def name
+    #   @name
+    # end
+    # attr_accessor :name
+    # attr_writer :name
+    # def name=(value)
+    #   @name = value
+    # end
+  
+# end
+
+# jay_z = Person.new
+# 
+
+
+class Cat
+
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+
+end
+
+
